f4fd36ed76082f718c4e14fdfa580209df236707
Eliott Veyrier <eliott.veyrier@umontpellier.fr>
2021-05-12 00:19:31 +0200

    doc: fully document the sequencer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 pub mod note;
+
+/// Sequence a tree-like structure ([sequencer::Atom]) into
+/// a sequence of simple instructions ([sequencer::Particle])
+#[warn(missing_docs)]
+pub mod sequencer;
+
 pub mod sync;
 pub mod synth;
-pub mod sequencer;
--- src/sequencer.rs
+++ src/sequencer.rs
@@ -1,22 +1,20 @@
-use std::{ iter::IntoIterator};
+use std::iter::IntoIterator;
 
 /// The base unit of sound : Either a duration of silence, or a duration and note.
 #[derive(Debug, PartialEq, Copy, Clone)]
 #[allow(dead_code)]
 pub enum Particle {
     /// A musical note to be played
-    /// 
-    /// `dur` : duration in nanoseconds
-    /// `note`: midi note in 0-127
     Note {
+        /// The duration of the note in nanoseconds
         dur: u64,
+        /// The pitch of the note, midi note in (0..127)
         pitch: u8,
     },
     /// A silence to be held
-    /// 
-    /// `dur` : duration in nanoseconds
-    Silence { 
-        dur: u64
+    Silence {
+        /// the duration in nanoseconds
+        dur: u64,
     },
 }
 
@@ -27,11 +25,15 @@ pub enum Particle {
 pub enum Atom {
     /// A single particle
     Singleton(Particle),
-    Cycle{
-        atoms : Vec<Atom>, 
-        len : u8,
-        times : u8
-    }
+    /// A loop of atoms
+    Cycle {
+        /// the atoms to loop
+        atoms: Vec<Atom>,
+        /// number of atoms in the loop
+        len: u8,
+        /// number of times to loop for
+        times: u8,
+    },
 }
 
 impl Atom {
@@ -40,72 +42,70 @@ impl Atom {
     pub fn particle_ref(&self) -> Option<&Particle> {
         match self {
             Atom::Singleton(particle) => Some(particle),
-            _ => None
+            _ => None,
         }
     }
     /// If the atom is a cycle, return a slice into its atoms.
     /// Returns `None` otherwise.
-    pub fn atoms_slice(&self) -> Option<&[Atom]> {
+    pub fn cycle_slice(&self) -> Option<&[Atom]> {
         match self {
-            Atom::Cycle { atoms, len : _, times : _ } => Some(atoms.as_slice()),
-            _ => None
+            Atom::Cycle {
+                atoms,
+                len: _,
+                times: _,
+            } => Some(atoms.as_slice()),
+            _ => None,
         }
     }
 
-    fn iter<'a, 'b : 'a>(&'b self) -> Box<dyn Iterator<Item=&'a Particle> + 'a> {
+    /// Returns an iterator that sequences the atom
+    pub fn iter<'a, 'b: 'a>(&'b self) -> Box<dyn Iterator<Item = &'a Particle> + 'a> {
         match self {
             Atom::Singleton(particle) => Box::new(std::iter::once(particle)),
-            Atom::Cycle{times, len : _, atoms : _} => {
+            Atom::Cycle {
+                times,
+                len: _,
+                atoms: _,
+            } => {
                 // TODO can probably be improved
-                let particles : Vec<_> = self.atoms_slice().unwrap().iter()
-                .map(|x| x.iter()).flatten().collect();
+                let particles: Vec<_> = self
+                    .cycle_slice()
+                    .unwrap()
+                    .iter()
+                    .map(|x| x.iter())
+                    .flatten()
+                    .collect();
 
-                Box::new(
-                    particles.repeat(*times as usize).into_iter()
-                )
+                Box::new(particles.repeat(*times as usize).into_iter())
             }
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const A : Particle = Particle::Note{dur:1,pitch:2};
-    const B : Particle = Particle::Note{dur:2,pitch:2};
-    const C : Particle = Particle::Note{dur:3,pitch:2};
-    const D : Particle = Particle::Note{dur:4,pitch:2};
+    const A: Particle = Particle::Note { dur: 1, pitch: 2 };
+    const B: Particle = Particle::Note { dur: 2, pitch: 2 };
+    const C: Particle = Particle::Note { dur: 3, pitch: 2 };
+    const D: Particle = Particle::Note { dur: 4, pitch: 2 };
 
     fn test_case() -> Atom {
         #[allow(non_snake_case)]
-        
         // loop3 - A - B - loop2 - C - D
-        Atom::Cycle{
+        Atom::Cycle {
             len: 3,
             times: 3,
             atoms: vec![
                 Atom::Singleton(A),
                 Atom::Singleton(B),
-                Atom::Cycle{
+                Atom::Cycle {
                     len: 2,
                     times: 4,
-                    atoms: vec![
-                        Atom::Singleton(C),
-                        Atom::Singleton(D),
-                    ]
-                }
-            ]
+                    atoms: vec![Atom::Singleton(C), Atom::Singleton(D)],
+                },
+            ],
         }
     }
 
@@ -113,8 +113,8 @@ mod tests {
     fn test_atom() {
         let case = test_case();
         assert_eq!(
-            case.atoms_slice().unwrap()[0..2],
-            [Atom::Singleton(A),Atom::Singleton(B)]
+            case.cycle_slice().unwrap()[0..2],
+            [Atom::Singleton(A), Atom::Singleton(B)]
         );
     }
     #[test]
@@ -124,5 +124,4 @@ mod tests {
             println!("{:?}", x);
         }
     }
-    
 }
